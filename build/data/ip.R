d49f845f96341802ddcbc38099b9cdc7714970b6
Alec Friedhoff <afriedhoff@gmail.com>
2019-06-08 18:23:21 -0400

    final data input check

--- build/data/ip.R
+++ build/data/ip.R
@@ -6,7 +6,7 @@ library(jsonlite)
 #to do: revise with latest data
 # check counts against increase/greater_inc variables
 
-file <- here("geog_jobs_data_final.xlsx")
+file <- here("geog_jobs_data_final2.xlsx")
 data <- read_xlsx(file) %>% rename(cbsa_old = cbsa) %>% mutate(cbsa = ifelse(cbsa_old == 19430, 19380, cbsa_old)) %>% filter(cntyfips=="00000")
 dayton <- filter(data, cbsa != cbsa_old)
 
@@ -17,7 +17,7 @@ types <- data %>% filter(cbsa == 99999, naics=="00", measure=="actual") %>% sele
 #trends
 trend_data <- data %>% filter(type=="TOTAL", naics=="00") %>% select("year", "measure", "pchange", "cbsa") %>% spread("measure", "pchange") %>% split(.$cbsa) %>% lapply(function(e){return(e %>% select(-cbsa))})
             
-tots_change <- tots %>% spread(key=measure, value=pchange) %>% split(.$cbsa)
+tots_change <- tots %>% spread(key="measure", value="pchange") %>% split(.$cbsa)
             
 seq0data <- toJSON(list(changes=tots_change), pretty=TRUE, digits=5, na="null")
 
@@ -26,6 +26,15 @@ sector15_ <- data %>% filter(type=="TOTAL", year==2015, !(cbsa %in% c(99997, 999
                 spread(measure, pchange)
 
 sector15counts <- sector15_ %>% mutate(p_=ifelse(actual>0,1,0), ge_=ifelse(actual > expected, 1, 0)) %>% filter(cbsa < 90000) %>% group_by(naics) %>% summarise(p=sum(p_), ge=sum(ge_), n=n())
+
+#test against data in sheet
+sector15_test <- data %>% filter(type=="TOTAL", year==2015, !(cbsa %in% c(99997, 99998)), !(naics %in% c("11", "21"))) %>% 
+  select(cbsa, naics, measure, pchange) %>% 
+  spread(measure, pchange) %>% 
+  mutate(p_=ifelse(actual>0,1,0), ge_=ifelse(actual > expected, 1, 0)) %>%
+  inner_join(data %>% filter(type=="TOTAL", year==2015, measure=="actual"), by=c("cbsa","naics")) 
+
+sector15_test %>% filter(p_ != increase | is.na(p_) | is.na(increase))
 
 county_ <- data %>% filter(cbsa < 90000, naics=="00", year==2015) %>% select("cbsa", "type", "measure","pchange") %>% spread(measure, pchange) 
 table(county_$type)
